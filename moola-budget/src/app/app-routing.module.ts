7e15f4666a1d6592b0336067c1bd0ed375d4272f
@Thendo <makherana@thedigitalacademy.co.za>
2021-10-11 16:46:44 +0200

    routerlink

--- moola-budget/src/app/app-routing.module.ts
+++ moola-budget/src/app/app-routing.module.ts
@@ -3,12 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { IconsComponent } from './components/icons/icons.component';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { BudgetPageComponent } from './pages/budget-page/budget-page.component';
+import { DiaryPageComponent } from './pages/diary-page/diary-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { QuotesPageComponent } from './pages/quotes-page/quotes-page.component';
+import { SavingsPageComponent } from './pages/savings-page/savings-page.component';
 
 const routes: Routes = [ 
   {path:'', redirectTo: 'home', pathMatch: 'full'},
   {path:'home', component: HomePageComponent, pathMatch: 'full'},
   {path: 'icons', component:IconsComponent,pathMatch: 'full'},
+  {path: '#', component:BudgetPageComponent,pathMatch: 'full'},
+  {path: 'diary', component:DiaryPageComponent,pathMatch: 'full'},
+  {path: 'quotes', component:QuotesPageComponent,pathMatch: 'full'},
+  {path: 'savings', component:SavingsPageComponent,pathMatch: 'full'},
   {path:'**', redirectTo: '', pathMatch: 'full'}
   
 ];
