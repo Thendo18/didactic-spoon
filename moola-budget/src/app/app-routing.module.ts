4ad0cdcc9779eecfb700de89480339a4bed3937f
@Thendo <makherana@thedigitalacademy.co.za>
2021-10-22 12:15:17 +0200

    routing components

--- moola-budget/src/app/app-routing.module.ts
+++ moola-budget/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddBudgetPlanComponent } from './components/add-budget-plan/add-budget-plan.component';
 import { IconsComponent } from './components/icons/icons.component';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PlanABudgetComponent } from './components/plan-a-budget/plan-a-budget.component';
 import { RegisterComponent } from './components/register/register.component';
 import { BudgetPageComponent } from './pages/budget-page/budget-page.component';
 import { DiaryPageComponent } from './pages/diary-page/diary-page.component';
@@ -19,6 +21,9 @@ const routes: Routes = [
   {path: 'diary', component:DiaryPageComponent,pathMatch: 'full'},
   {path: 'quotes', component:QuotesPageComponent,pathMatch: 'full'},
   {path: 'savings', component:SavingsPageComponent,pathMatch: 'full'},
+  {path: 'add', component:AddBudgetPlanComponent,pathMatch: 'full'},
+  {path: 'plan', component:PlanABudgetComponent,pathMatch: 'full'},
+
   {path: 'register', component:RegisterComponent,pathMatch: 'full'},
   {path: 'wallet', component:SavingsPageComponent,pathMatch: 'full'},
   {path:'**', redirectTo: '', pathMatch: 'full'}
