5a6153382d73064f70b39b98d0caac56418b94e1
@Thendo <makherana@thedigitalacademy.co.za>
2021-10-28 12:50:18 +0200

    service

--- moola-budget-backend/src/app.module.ts
+++ moola-budget-backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BudgetController } from './budget/budget.controller';
+import { BudgetService } from './budget/budget.service';
 
 @Module({
   imports: [],
   controllers: [AppController, BudgetController],
-  providers: [AppService],
+  providers: [AppService, BudgetService],
 })
 export class AppModule {}
--- moola-budget-backend/src/budget/budget.controller.ts
+++ moola-budget-backend/src/budget/budget.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Delete,Body } from '@nestjs/common';
+import { Controller, Get, Post, Delete,Body, Param, Put } from '@nestjs/common';
 import { CreateBudgetDto } from './dto/create-budget.dto';
 @Controller('budget')
 export class BudgetController {
@@ -7,9 +7,23 @@ export class BudgetController {
     findAll(): string{
         return 'get budget';
     }
-    
+
     @Post()
     create(@Body()createBudgetDto: CreateBudgetDto): string{
-      return `Name: ${createBudgetDto.name} Period: ${createBudgetDto.period}`; 
+      return `Name: ${createBudgetDto.name} Period: ${createBudgetDto.period} Amount: ${createBudgetDto.amount}`; 
     }
+
+    @Delete(':id')
+delete(@Param('id') id): string{
+
+return `Delete &${id}`;
+}
+
+
+@Put(':id')
+    update(@Body() updateBudgetDto: CreateBudgetDto, @Param('id')  id): string{
+        return `Update ${id} -Name: ${updateBudgetDto.name} Period: ${updateBudgetDto.period} Amount: ${updateBudgetDto.amount}`;
+}
+
+
 }
