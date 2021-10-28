ff72e774f3f116ebd54b6f654d2be14722102fe9
@Thendo <makherana@thedigitalacademy.co.za>
2021-10-28 15:56:42 +0200

    changes

--- moola-budget-backend/src/app.module.ts
+++ moola-budget-backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BudgetController } from './budget/budget.controller';
 import { BudgetService } from './budget/budget.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [],
+  imports: MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController, BudgetController],
   providers: [AppService, BudgetService],
 })
