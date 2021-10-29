164e1ce3e94d45df8f80caa11c8c5df37e59e0a0
@Thendo <makherana@thedigitalacademy.co.za>
2021-10-29 08:25:48 +0200

    backend

--- moola-budget-backend/src/budget/budget.controller.ts
+++ moola-budget-backend/src/budget/budget.controller.ts
@@ -1,37 +1,40 @@
-import { Controller, Get, Post, Delete,Body, Param, Put } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Body,
+  Param,
+  Put,
+} from '@nestjs/common';
 import { CreateBudgetDto } from './dto/create-budget.dto';
 import { BudgetService } from './budget.service';
 import { budget } from './interfaces/budget.interface';
 @Controller('budget')
-
 export class BudgetController {
-    constructor (private readonly  budgetService: BudgetService) {}
-    @Get()
-    findAll(): budget[]{
-        return this.budgetService.findAll();
-    }
+  constructor(private readonly budgetService: BudgetService) {}
+  @Get()
+  async findAll(): Promise<budget[]> {
+    return this.budgetService.findAll();
+  }
 
-@Get(':id')
-findOne(@Param('id') id): budget{
+  @Get(':id')
+  async findOne(@Param('id') id): Promise<budget> {
     return this.budgetService.findOne(id);
-}
+  }
 
-    @Post()
-    create(@Body()createBudgetDto: CreateBudgetDto): string{
-      return `Name: ${createBudgetDto.name} Period: ${createBudgetDto.period} Amount: ${createBudgetDto.amount}`; 
-    }
+  @Post()
+  create(@Body() createBudgetDto: CreateBudgetDto): string {
+    return `Name: ${createBudgetDto.name} Period: ${createBudgetDto.period} Amount: ${createBudgetDto.amount}`;
+  }
 
-    @Delete(':id')
-delete(@Param('id') id): string{
+  @Delete(':id')
+  delete(@Param('id') id): string {
+    return `Delete &${id}`;
+  }
 
-return `Delete &${id}`;
-}
-
-
-@Put(':id')
-    update(@Body() updateBudgetDto: CreateBudgetDto, @Param('id')  id): string{
-        return `Update ${id} -Name: ${updateBudgetDto.name} Period: ${updateBudgetDto.period} Amount: ${updateBudgetDto.amount}`;
-}
-
-
+  @Put(':id')
+  update(@Body() updateBudgetDto: CreateBudgetDto, @Param('id') id): string {
+    return `Update ${id} -Name: ${updateBudgetDto.name} Period: ${updateBudgetDto.period} Amount: ${updateBudgetDto.amount}`;
+  }
 }
