17cc555d8ec5de052cdcdb9653069759bc668af3
Thendo <thendomakherana@gmail.com>
2021-10-31 10:38:57 +0200

    importing budget service

--- moola-budget/src/app/components/savings/savings.component.ts
+++ moola-budget/src/app/components/savings/savings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BudgetService } from 'src/app/services/budget.service';
 
 @Component({
   selector: 'app-savings',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SavingsComponent implements OnInit {
 
-  constructor() { }
+  details:any;
+  constructor(private budgetService:BudgetService) { }
 
   ngOnInit(): void {
   }
