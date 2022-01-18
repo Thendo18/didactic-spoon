4716700bba52170618891f6ad93c238e6d8497e3
Thendo <thendomakherana@gmail.com>
2022-01-18 12:59:28 +0200

    securing url

--- moola-budget-backend/src/budget/config/keys.ts
+++ moola-budget-backend/src/budget/config/keys.ts
@@ -1,3 +1,3 @@
 export default {
-    mongoURI: 'mongodb+srv://Thendo:24681012@cluster0.5rkio.mongodb.net/budget?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://Thendo:password@cluster0.5rkio.mongodb.net/budget?retryWrites=true&w=majority'
 }
